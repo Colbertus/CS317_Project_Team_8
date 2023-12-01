bbea41b7572729f75f9cb6c1c6f5070e4b27239d
Kable <rcb0027@uah.edu>
2023-12-01 17:28:21 -0600

    Create soundTable.lua

---
+++ CS317_Project_Team_8-main/CS317_Project_Team_8-main/CS317_Project_Team_8-main/CS317_Project_Team_8-main/CS371_Project_Team8/soundTable.lua
@@ -0,0 +1,8 @@
+-- Instantiate the sound table for the collistion sounds 
+----------------------------------------
+local soundTable = {
+		bulletToEnemy = audio.loadSound("bulletToEnemy.wav"), 
+		bulletToBoss = audio.loadSound("bulletToBoss.wav"),
+		enemyBullet = audio.loadSound("enemyBulletToPlayer.wav"),
+		bossBullet = audio.loadSound("bossBulletToPlayer.wav")
+	}
